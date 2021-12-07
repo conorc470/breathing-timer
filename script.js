f094f60f676427cb02a03f000bdc4c0f67382476
Conor Clarke <31772275+conorc470@users.noreply.github.com>
2021-12-07 16:58:54 +0000

    css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+const circle = document.getElementById('circle');
 
 const totalTime = 11000;
 const breatheTime = (totalTime / 2);
@@ -9,12 +10,13 @@ breathAnimation();
 function breathAnimation() {
   text.innerText = 'Breathe In...';
   container.className = 'container grow';
+	text.style.color = '#377dce';
 
   setTimeout(() => {
-
     setTimeout(() => {
       text.innerText = 'Breathe Out...';
       container.className = 'container shrink';
+			text.style.color = '#e9aa0a';
     }, 0);
   }, breatheTime);
 }
