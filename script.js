f5b39b131cf4e2f2cab71cbde25b47a1c4f9209e
Conor Clarke <31772275+conorc470@users.noreply.github.com>
2021-12-07 16:31:21 +0000

    animation working

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 
-const totalTime = 7500;
-const breatheTime = (totalTime / 5) * 2;
-const holdTime = totalTime / 5;
+const totalTime = 11000;
+const breatheTime = (totalTime / 2);
 
 breathAnimation();
 
 function breathAnimation() {
-  text.innerText = 'Breathe In!';
+  text.innerText = 'Breathe In...';
   container.className = 'container grow';
 
   setTimeout(() => {
-    text.innerText = 'Hold';
 
     setTimeout(() => {
-      text.innerText = 'Breathe Out!';
+      text.innerText = 'Breathe Out...';
       container.className = 'container shrink';
-    }, holdTime);
+    }, 0);
   }, breatheTime);
 }
 
