93c17cb4634305d051a282049f77efb8f3ee96cd
Conor Clarke <31772275+conorc470@users.noreply.github.com>
2021-12-08 11:05:27 +0000

    styling

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const breatheTime = (totalTime / 2);
 breathAnimation();
 
 function breathAnimation() {
-  text.innerText = 'Breathe In...';
+  text.innerText = 'breathe in';
   container.className = 'container grow';
 	text.style.color = '#377dce';
 
   setTimeout(() => {
     setTimeout(() => {
-      text.innerText = 'Breathe Out...';
+      text.innerText = 'breathe out';
       container.className = 'container shrink';
 			text.style.color = '#e9aa0a';
     }, 0);
